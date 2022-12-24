8402c35bc4de40a0e67f9ceabadf0dc297df9970
Alois Vollmaier <alois.vollmaier.extern@rewe-group.com>
2022-12-24 15:23:09 +0100

    dockerfiles for all projects

--- edge-service/build.gradle.kts
+++ edge-service/build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
     runtimeOnly("io.zipkin.reporter2:zipkin-reporter-brave")
     implementation(libs.bundles.j2html)
     implementation(libs.bundles.kotlinLogging)
-
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
